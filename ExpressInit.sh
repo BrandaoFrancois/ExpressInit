e8a39c95c7c3c7b6210880271d8a0f3bf38697bd
Brandao Francois <francois.xavier.brandao@gmail.com>
2021-05-06 23:50:34 +0200

    Some commands needed for the program

--- ExpressInit.sh
+++ ExpressInit.sh
@@ -2,3 +2,19 @@
 #
 
 echo "Init project ..."
+
+sudo apt-get update
+
+# Installation de Nodejs version 15
+wget -qO- https://deb.nodesource.com/setup_15.x | sudo -E bash -
+sudo apt-get install -y nodejs npm
+
+npm init
+npm install express --save
+npm install express-session --save
+npm install body-parser --save
+npm install helmet --save
+npm install log4js --save
+npm install bcrypt --save
+# Base de donnee mysql
+npm install mysql --save
